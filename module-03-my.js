c5733565e63c61257206a2714f4da0805bbb50a2
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-05-29 13:18:22 +0300

    upd `module-03-my.js` test the `randomIndexArrary` empty elements

--- module-03-my.js
+++ module-03-my.js
@@ -1,5 +1,19 @@
 // crazy
-const randomIndexArrary = ["init"];
-randomIndexArrary[3] = "a";
-randomIndexArrary[5] = "element";
-console.table(randomIndexArrary)
+const randomIndexArrary = ['init'];
+randomIndexArrary[3] = 'a';
+randomIndexArrary[5] = 'element';
+console.log(randomIndexArrary);
+
+// empty elements become undefined in the end
+const normArray = [...randomIndexArrary];
+console.log(normArray);
+
+for (const item of randomIndexArrary) {
+  if (typeof item === 'undefined') {
+    console.log('>skip undefined');
+    continue;
+  }
+  console.log(item);
+}
+
+// but dont create arrays like that and it also shouldn't happen irl
