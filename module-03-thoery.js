d2167be1e0a81493341c19f7c08c0f05039ec7b5
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-05-30 23:10:05 +0300

    upd `module-03-theory.js`

--- module-03-thoery.js
+++ module-03-thoery.js
@@ -54,3 +54,117 @@ console.log(getExtremeElements(['apple']));
 
 // ================
 
+const words = ['JavaScript', 'is', 'amazing'];
+console.log(words.join('')); // 'JavaScriptisamazing'
+console.log(words.join(' ')); // 'JavaScript is amazing'
+console.log(words.join('-')); // 'JavaScript-is-amazing'
+console.log(words.join('_+_')); // 'JavaScript-is-amazing'
+
+function getLength(array) {
+  return array.join('').length;
+}
+console.log(getLength(['Mango', 'hurries', 'to', 'the', 'train']));
+
+// ================
+
+const nameUser = 'Mango';
+const copy = nameUser.split();
+console.log(copy); // ["Mango"]
+const letters = nameUser.split('');
+console.log(letters); // ["M", "a", "n", "g", "o"]
+
+const message = 'JavaScript essentials';
+const words2 = message.split(' ');
+console.log(words2); // ["JavaScript", "essentials"]
+
+const slug = 'amazing-french-recipes';
+const slugParts = slug.split('-');
+console.log(slugParts); // ["amazing", "french", "recipes"]
+
+function calculateEngravingPrice(message, pricePerWord) {
+  const wordsAmount = message.split(' ').length;
+  return wordsAmount * pricePerWord;
+}
+console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+
+// ================
+
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, -1);
+const lastThreeEls = fruits.slice(-3);
+
+console.log(firstTwoEls);
+console.log(nonExtremeEls);
+console.log(lastThreeEls);
+
+// ================
+
+const firstArray = ['Mercury', 'Venus'];
+const secondArray = ['Mars', 'Jupiter'];
+const thirdArray = ['Saturn', 'Neptune'];
+const notArray = 'Earth'; //also works
+
+console.log(firstArray.concat(secondArray, thirdArray, notArray));
+
+// ================
+
+function calculateTotalPrice(order) {
+  let sum = 0;
+
+  for (let i = 0; i < order.length; i++) {
+    sum += order[i];
+  }
+
+  return sum;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// ================
+
+function sum1(a, b) {
+  console.log(arguments);
+  return a + b;
+}
+console.log(sum1(2, 5));
+
+function sum2(a, b) {
+  // a, b - aunused
+  console.log(arguments);
+
+  let total = 0;
+  for (const arg of arguments) {
+    total += arg;
+  }
+  return total;
+}
+console.log(sum2(2, 5, 7, 8));
+
+// ================
+
+function createReversedArray() {
+  return Array.from(arguments).toReversed();
+}
+
+console.log(createReversedArray(12, 85, 37, 4));
+console.log(createReversedArray());
+
+// ================
+
+function count(from = 0, to, step = 1) {
+  console.log(`from: ${from}, to: ${to}, step: ${step}`);
+
+  let string = '';
+  for (let i = from; i <= to; i += step) {
+    string += i + ';';
+  }
+  console.log(string);
+}
+
+count(1, 15, 4); // "from: 1, to: 15, step: 4"
+count(1, 15); // "from: 1, to: 15, step: 1"
+count(); // from: 0, to: undefined, step: 1
