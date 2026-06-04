46743bdaf28048055cc072582bb4d92413f91793
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-06-04 21:40:42 +0300

    upd `module-04-theory.js`

--- module-04-thoery.js
+++ module-04-thoery.js
@@ -148,3 +148,21 @@ const user = {
 console.log(user);
 console.log(user.name);
 console.log(user.namepostfix);
+
+// ================
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = [];
+const values = [];
+
+for (const key in apartment){
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+console.log(keys, values)
